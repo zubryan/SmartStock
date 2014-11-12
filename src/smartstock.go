3f3583f6e21ebd87fb6b59049856b895f0c7d1a7
域遥 <weigang.zwg@alibaba-inc.com>
2014-11-12 23:20:39 +0800

    added stock struct

--- src/smartstock.go
+++ src/smartstock.go
@@ -2,30 +2,54 @@ package main
 
 import (
 	"client"
+	"encoding/json"
 	"fmt"
-	//"io/ioutil"
-	//"net/http"
+	"io/ioutil"
+	"net/http"
 )
 
+type Stock struct {
+	Timestamp      int64
+	Ticker         string
+	ExchangeCD     string
+	DataDate       string
+	DataTime       string
+	ShortNM        string
+	CurrencyCD     string
+	PrevClosePrice float64
+	OpenPrice      float64
+	Volume         int64
+	Value          float64
+	Deal           int32
+	HighPrice      float64
+	LowPrice       float64
+	LastPrice      float64
+}
+
+type Stockslice struct {
+	Data []Stock
+}
+
 func main() {
-	/*
-		req, err := http.NewRequest("GET", "https://gw.wmcloud.com/data/market/1.0.0/getTickRTSnapshot.csv", nil)
+	var stock Stockslice
+	req, err := http.NewRequest("GET", "https://gw.wmcloud.com/data/market/1.0.0/getTickRTSnapshot.json?field=dataDate,dataTime,shortNM,currencyCD,prevClosePrice,openPrice,volume,value,deal,highPrice,lowPrice,lastPrice", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Authorization", "Bearer f637816e9303cea3c12981eee33f76ffd090ce9ba1599bac4853fba65a226243")
+
+	httpClient := &http.Client{}
+
+	resp, _ := httpClient.Do(req)
+
+	if resp.StatusCode == 200 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		err := json.Unmarshal(body, &stock)
 		if err != nil {
-			fmt.Println("Error:", err)
 			panic(err)
 		}
-		req.Header.Add("Authorization", "Bearer f637816e9303cea3c12981eee33f76ffd090ce9ba1599bac4853fba65a226243")
-
-		httpClient := &http.Client{}
-
-		resp, _ := httpClient.Do(req)
-
-		if resp.StatusCode == 200 {
-			body, _ := ioutil.ReadAll(resp.Body)
-
-			fmt.Println(string(body))
-		}
-	*/
+		fmt.Println(stock)
+	}
 
 	c, err := client.NewClient(&client.ClientConfig{
 		Host:     "192.168.129.136:8086",
