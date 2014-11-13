969920717189713a0eb733633bbb2493a93e9496
域遥 <weigang.zwg@alibaba-inc.com>
2014-11-14 00:21:25 +0800

    Fixed a bug about channel

--- src/smartstock.go
+++ src/smartstock.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"client"
+	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/influxdb/influxdb/client"
+	"github.com/larspensjo/config"
 	"io/ioutil"
+	"math"
 	"net/http"
+	"os"
+)
+
+var (
+	configFile = flag.String("configfile", "config.ini", "General configuration file")
 )
 
 type Stock struct {
@@ -30,13 +39,16 @@ type Stockslice struct {
 	Data []Stock
 }
 
-func main() {
+var APICONF = make(map[string]string)
+var DBCONF = make(map[string]string)
+
+var CONSTMOD = 2
+
+func importData(auth string, url string, ch chan int) {
 	var stock Stockslice
-	req, err := http.NewRequest("GET", "https://gw.wmcloud.com/data/market/1.0.0/getTickRTSnapshot.json?field=dataDate,dataTime,shortNM,currencyCD,prevClosePrice,openPrice,volume,value,deal,highPrice,lowPrice,lastPrice", nil)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Add("Authorization", "Bearer f637816e9303cea3c12981eee33f76ffd090ce9ba1599bac4853fba65a226243")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Authorization", "Bearer "+auth)
 
 	httpClient := &http.Client{}
 
@@ -44,34 +56,114 @@ func main() {
 
 	if resp.StatusCode == 200 {
 		body, _ := ioutil.ReadAll(resp.Body)
-		err := json.Unmarshal(body, &stock)
-		if err != nil {
-			panic(err)
-		}
+		json.Unmarshal(body, &stock)
 
-		var dataSh []Stock
-		for _, v := range stock.Data {
-			if v.ExchangeCD == "XSHG" {
-				dataSh = append(dataSh, v)
+		c, _ := client.NewClient(&client.ClientConfig{
+			Host:     "192.168.129.136:8086",
+			Username: "root",
+			Password: "root",
+			Database: "smartstock",
+		})
+
+		for j := 0; j < len(stock.Data); j++ {
+			name := "mktdata." + stock.Data[j].Ticker + "." + stock.Data[j].ExchangeCD
+			series := &client.Series{
+				Name:    name,
+				Columns: []string{"ticker.exchange", "dataDate", "dataTime", "lastPrice", "volume"},
+				Points: [][]interface{}{
+					{stock.Data[j].Ticker + "." + stock.Data[j].ExchangeCD, stock.Data[j].DataDate, stock.Data[j].DataTime, stock.Data[j].LastPrice, stock.Data[j].Volume},
+				},
+			}
+			c.WriteSeries([]*client.Series{series})
+		}
+	}
+	ch <- 1
+}
+
+func main() {
+	cfg, err := config.ReadDefault(*configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	if cfg.HasSection("API") {
+		section, err := cfg.SectionOptions("API")
+		if err == nil {
+			for _, v := range section {
+				options, err := cfg.String("API", v)
+				if err == nil {
+					APICONF[v] = options
+				}
 			}
 		}
-		fmt.Println(len(dataSh))
 	}
 
-	c, err := client.NewClient(&client.ClientConfig{
-		Host:     "192.168.129.136:8086",
-		Username: "root",
-		Password: "root",
-		Database: "smartstock",
-	})
+	if cfg.HasSection("DB") {
+		section, err := cfg.SectionOptions("DB")
+		if err == nil {
+			for _, v := range section {
+				options, err := cfg.String("DB", v)
+				if err == nil {
+					DBCONF[v] = options
+				}
+			}
+		}
+	}
+
+	fin, err := os.Open("../data/stocklist")
+	defer fin.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	result, err := c.Query("select * from cpu_idle")
-	if err != nil {
-		panic(err)
+	var stockSecIds []string
+	r := bufio.NewReader(fin)
+	s, err := r.ReadString('\n')
+	for err == nil {
+		stockSecIds = append(stockSecIds, s)
+		s, err = r.ReadString('\n')
 	}
 
-	fmt.Println(result[0].GetPoints())
+	var stockSecId []string = stockSecIds[:9]
+	stockLen := len(stockSecId)
+
+	modR := int(math.Mod(float64(stockLen), float64(CONSTMOD)))
+	var groupNum int
+	if modR > 0 {
+		groupNum = (stockLen / CONSTMOD) + 1
+	} else {
+		groupNum = (stockLen / CONSTMOD)
+	}
+
+	var securityId string
+	m := 0
+	n := 0
+	chs := make([]chan int, groupNum)
+	for i := 0; i < len(stockSecId); i++ {
+		m++
+		securityId = stockSecId[i][0:len(stockSecId[i])-1] + "," + securityId
+		if m%CONSTMOD == 0 {
+			var url = APICONF["url"] + "/" + APICONF["market"] + "/" + APICONF["version"] + "/getTickRTSnapshot.json?securityID=" + securityId + "&field=dataDate,dataTime,shortNM,currencyCD,prevClosePrice,openPrice,volume,value,deal,highPrice,lowPrice,lastPrice"
+			fmt.Println(url)
+			chs[n] = make(chan int)
+			go importData(APICONF["auth"], url, chs[n])
+			securityId = ""
+			n++
+		}
+	}
+
+	securityId = ""
+	if modR > 0 {
+		for p := 0; p < modR; p++ {
+			securityId = stockSecId[stockLen-modR+p][0:len(stockSecId[stockLen-modR+p])-1] + "," + securityId
+		}
+		var url = APICONF["url"] + "/" + APICONF["market"] + "/" + APICONF["version"] + "/getTickRTSnapshot.json?securityID=" + securityId + "&field=dataDate,dataTime,shortNM,currencyCD,prevClosePrice,openPrice,volume,value,deal,highPrice,lowPrice,lastPrice"
+		fmt.Println(url)
+		chs[n] = make(chan int)
+		go importData(APICONF["auth"], url, chs[n])
+	}
+
+	for _, ch := range chs {
+		<-ch
+	}
 }
