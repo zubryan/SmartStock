ef7dce5100e069a89eba47f8362ea379a0fd12d4
miuzel <miuzel@gmail.com>
2014-11-26 22:46:11 +0800

    other dependencies

---
+++ src/github.com/dimdin/decimal/README.md
@@ -0,0 +1,42 @@
+decimal
+=======
+
+go decimal package suitable for financial and monetary calculations
+
+## Installation
+
+    go get github.com/dimdin/decimal
+
+## Documentation
+
+[Documentation and usage examples](http://godoc.org/github.com/dimdin/decimal)
+
+## Usage
+
+```go
+import "github.com/dimdin/decimal"
+
+    var x, y decimal.Dec
+    x.SetString("100")
+    y.SetString("3")
+    x.Div(&x, &y, 2)
+    fmt.Println(x)
+```
+Output:
+
+    33.33
+
+## Features
+
+- 38 decimal digits precision implemented with an 128 bit integer scaled by a power of ten.
+- Fast addition, subtraction, multiplication, division and power operations.
+- Arithmetic overflow detection that panics.
+- Can be scanned directly from database/sql query results.
+- Can be used directly in database/sql Query and Exec parameters.
+- Methods are in the math/big form `func (z *Dec) Op(x, y *Dec) *Dec` with the result as receiver.
+- Arithmetic half up rounding.
+- Test suite with more than 90% code coverage.
+
+## License
+
+Use of this source code is governed by BSD 2-clause license that can be found in the LICENSE file.
---
+++ src/github.com/dimdin/decimal/nulldec_test.go
@@ -0,0 +1,322 @@
+// Copyright 2014 Dimitris Dinodimos. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package decimal
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestSetBytes(t *testing.T) {
+	values := [][]byte{
+		nil,
+		[]byte("0"),
+		[]byte("10"),
+		[]byte("-1"),
+	}
+	for _, a := range values {
+		var x NullDec
+		x.SetBytes(a)
+		v := x.Bytes()
+		if string(v) != string(a) {
+			t.Errorf("Failed to SetBytes %s, got %s", string(a), string(v))
+		}
+	}
+}
+
+func TestNullSign(t *testing.T) {
+	values := []struct {
+		x string
+		s int
+	}{
+		{"1.2", 1},
+		{"-1.2", -1},
+		{"0.0", 0},
+		{"", 0},
+	}
+	for _, a := range values {
+		var x NullDec
+		x.SetString(a.x)
+		if x.Sign() != a.s {
+			t.Errorf("sign of %s got %d want %d", a.x, x.Sign(), a.s)
+		}
+	}
+}
+
+func TestNullNeg(t *testing.T) {
+	values := []struct {
+		x string
+		y string
+	}{
+		{"", ""},
+		{"0", "0"},
+		{"-0.1", "0.1"},
+		{"0.1", "-0.1"},
+	}
+	for _, a := range values {
+		var x, y NullDec
+		x.SetString(a.x)
+		y.Neg(&x)
+		if y.String() != a.y {
+			t.Errorf("neg of %s got %s want %s", a.x, y.String(), a.y)
+		}
+	}
+}
+
+func TestNullAbs(t *testing.T) {
+	values := []struct {
+		x string
+		y string
+	}{
+		{"1.2", "1.2"},
+		{"-1.2", "1.2"},
+		{"0", "0"},
+		{"", ""},
+	}
+	for _, a := range values {
+		var x, y NullDec
+		x.SetString(a.x)
+		y.Abs(&x)
+		if y.String() != a.y {
+			t.Errorf("abs of %s got %s want %s", a.x, y.String(), a.y)
+		}
+	}
+}
+
+func TestNullCmp(t *testing.T) {
+	values := []struct {
+		x string
+		y string
+		c int
+	}{
+		{"1.2", "1.2", 0},
+		{"0.1", "0.01", 1},
+		{"0.01", "0.1", -1},
+		{"", "0.1", 0},
+		{"0.1", "", 0},
+	}
+	for _, a := range values {
+		var x, y NullDec
+		x.SetString(a.x)
+		y.SetString(a.y)
+		c := x.Cmp(&y)
+		if c != a.c {
+			t.Errorf("cmp %s <> %s got %d want %d", a.x, a.y, c, a.c)
+		}
+	}
+}
+
+func TestNullAdd(t *testing.T) {
+	values := []struct {
+		x string
+		y string
+		z string
+	}{
+		{"1.2", "1.2", "2.4"},
+		{"-1.2", "1.2", "0.0"},
+		{"0", "0", "0"},
+		{"", "1", ""},
+		{"1", "", ""},
+	}
+	for _, a := range values {
+		var x, y, z NullDec
+		x.SetString(a.x)
+		y.SetString(a.y)
+		z.Add(&x, &y)
+		if a.z != z.String() {
+			t.Errorf("%s + %s got %s want %s", a.x, a.y, z.String(), a.z)
+		}
+	}
+}
+
+func TestNullSub(t *testing.T) {
+	values := []struct {
+		x string
+		y string
+		z string
+	}{
+		{"1.2", "1.2", "0.0"},
+		{"-1.2", "1.2", "-2.4"},
+		{"0", "0", "0"},
+		{"", "1", ""},
+		{"1", "", ""},
+	}
+	for _, a := range values {
+		var x, y, z NullDec
+		x.SetString(a.x)
+		y.SetString(a.y)
+		z.Sub(&x, &y)
+		if a.z != z.String() {
+			t.Errorf("%s - %s got %s want %s", a.x, a.y, z.String(), a.z)
+		}
+	}
+}
+
+func TestNullMul(t *testing.T) {
+	values := []struct {
+		x string
+		y string
+		z string
+	}{
+		{"1.2", "0", "0.0"},
+		{"1.2", "1", "1.2"},
+		{"1.2", "-1", "-1.2"},
+		{"1.2", "2", "2.4"},
+		{"1", "", ""},
+		{"", "1", ""},
+	}
+	for _, a := range values {
+		var x, y, z NullDec
+		x.SetString(a.x)
+		y.SetString(a.y)
+		z.Mul(&x, &y)
+		if a.z != z.String() {
+			t.Errorf("%s * %s got %s want %s", a.x, a.y, z.String(), a.z)
+		}
+	}
+}
+
+func TestNullRound(t *testing.T) {
+	values := []struct {
+		x     string
+		scale uint8
+		r     string
+	}{
+		{"1.234", 2, "1.23"},
+		{"", 2, ""},
+	}
+	for i, a := range values {
+		var x NullDec
+		x.SetString(a.x)
+		x.Round(a.scale)
+		if x.String() != a.r {
+			t.Errorf("#%d got %s want %s", i, x, a.r)
+		}
+	}
+}
+
+func TestNullDiv(t *testing.T) {
+	values := []struct {
+		x, y  string
+		scale uint8
+		r     string
+	}{
+		{"10", "1", 2, "10.00"},
+		{"10", "2", 2, "5.00"},
+		{"", "2", 0, ""},
+		{"10", "", 0, ""},
+	}
+	for _, a := range values {
+		var x, y, z NullDec
+		x.SetString(a.x)
+		y.SetString(a.y)
+		z.Div(&x, &y, a.scale)
+		if z.String() != a.r {
+			t.Errorf("%s / %s round %d got %s want %s", a.x, a.y, a.scale, z.String(), a.r)
+		}
+	}
+}
+
+func TestNullPower(t *testing.T) {
+	values := []struct {
+		x string
+		n int
+		r string
+	}{
+		{"2", 0, "1"},
+		{"2", 1, "2"},
+		{"2", 2, "4"},
+		{"", 2, ""},
+	}
+	for _, a := range values {
+		var x, y NullDec
+		x.SetString(a.x)
+		y.Power(&x, a.n)
+		if y.String() != a.r {
+			t.Errorf("%s^%d got %s want %s", a.x, a.n, y.String(), a.r)
+		}
+		if x.String() != a.x {
+			t.Errorf("%s^%d alters %s to %s", a.x, a.n, a.x, x.String())
+		}
+	}
+}
+
+func TestNullFloat64(t *testing.T) {
+	values := []string{
+		"12.34",
+		"-12.34",
+		"0",
+		"1",
+		"-1",
+	}
+	for _, v := range values {
+		var d NullDec
+		d.SetString(v)
+		s := strconv.FormatFloat(d.Float64(), 'f', -1, 64)
+		if v != s {
+			t.Errorf("expecting %s got %s", v, s)
+		}
+	}
+}
+
+func TestNullSetFloat64(t *testing.T) {
+	values := []float64{
+		// 17 significant digits
+		9876543210987654000000000000,
+		-9876543210987654000000000000,
+		9876543210.987654,
+		-9876543210.987654,
+		1234567890123456700000000000,
+		-1234567890123456700000000000,
+	}
+	for _, v := range values {
+		var d NullDec
+		d.SetFloat64(v)
+		f := d.Float64()
+		if v != f {
+			t.Errorf("expecting %f got %f", v, f)
+		}
+	}
+}
+
+func TestNullValue(t *testing.T) {
+	var d NullDec
+	x, err := d.Value()
+	if err != nil {
+		t.Errorf("failed to return Value: %s", err.Error())
+	}
+	if x != nil {
+		t.Errorf("failed to return nil Value")
+	}
+	d.SetString("-12.34")
+	x, err = d.Value()
+	if err != nil {
+		t.Errorf("failed to return Value: %s", err.Error())
+	}
+	if string(x.([]byte)) != d.String() {
+		t.Errorf("failed to return %s, got %s", d, x)
+	}
+}
+
+func TestNullScan(t *testing.T) {
+	s := "-12.34"
+	var x interface{} = []byte(s)
+	var d NullDec
+	err := d.Scan(x)
+	if err != nil {
+		t.Errorf("failed to Scan: %s", err.Error())
+	}
+	if d.String() != s {
+		t.Errorf("failed to scan %s, got %s", x, d)
+	}
+	x = nil
+	err = d.Scan(x)
+	if err != nil {
+		t.Errorf("failed to Scan: %s", err.Error())
+	}
+	if !d.Null() {
+		t.Errorf("failed to scan null")
+	}
+}
