33e046e63225b77c8919d4f266d917e6dc410cbc
miuzel <miuzel@gmail.com>
2014-12-02 13:26:31 +0800

    order asc for mktdata fetching

--- src/smartstock_calc.go
+++ src/smartstock_calc.go
@@ -461,7 +461,7 @@ func HaveAlerts(Idx int) bool {
 	var haveAlerts bool = false
 
 	query := fmt.Sprintf("select dataDate,dataTime,lastPrice,price_change_percentage,volume "+
-		"from mktdata.%s where time > %d", pRef.ticker_exchange, pRef.lasttime)
+		"from mktdata.%s where time > %d order asc", pRef.ticker_exchange, pRef.lasttime)
 
 	if DEBUGMODE {
 		Logger.Println(query)
